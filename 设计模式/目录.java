e9aa5e3ba0ed8aef00ff01a51fedf35b8c72491d
houna <717578983@qq.com>
2022-10-08 18:20:24 +0800

    提交

---
+++ 设计模式/目录.java
@@ -0,0 +1,119 @@
+
+
+
+这个作者的设计模式讲的很不错 https://www.jianshu.com/p/a707662abd0a
+
+VIP课题：​设计模式-03 “花式”对象创建的必要 构建、原型模式的应用
+课程内容：
+1、Builder模式的应用意义及使用案例；
+2、原型模式的本质与“深”“浅”拷贝的差异；
+3、对于行为性设计模式的本质理解；
+4、Android动画中对于策略模式的应用。
+
+预习资料链接：https://www.processon.com/view/link/630294121efad47d1245a158
+
+
+
+VIP课题：chapter-04 行为型模式的开端 策略、状态、责任链
+课题内容：
+1、行为型设计模式所解决的问题；
+2、策略模式模式的应用场景；
+3、Android下动画插值器的应用思路理解；
+4、状态模式与策略模式异同及使用场景；
+5、责任链模式的具体业务实例应用。
+
+预习资料链接：https://www.processon.com/view/link/630688451e0853072b9a10e1
+
+关于策略模式：有人说，“感觉这个就是多态的应用吧”。我其实也有这样的感觉
+
+// ============ 该看责任链模式了
+// 10.8 周六  
+// 1 把第三个看完  1h（11:00开始）
+// 2 10分钟左右，看了第四个的 20%左右
+// 10.9 周日 
+// 早上=== 
+// 1 看第四个的一半 (1h)
+// 晚上 
+// 1 看完第四个
+// ====== 基本每天早上+晚上，可以至少看一个
+
+
+VIP课题：chapter-05 解释器、命令、观察者
+课程内容：
+1、解释器模式的业务解决方案；
+2、命令模式的应用；
+3、观察者模式的应用场景与弊端思考；
+4、Android体系中的观察者应用。
+
+预习资料链接：https://www.processon.com/view/link/6307c47a63768906ff654774
+
+
+
+VIP课题：chapter-06 行为模式的总结 模板方法、备忘录、迭代器 
+上课时间：2022/8/29 20:30-22:30 
+上课讲师：kerwin老师 
+课程链接：https://ke.qq.com/webcourse/index.html?r=1661759219571#cid=2024962&term_id=104592243&taid=13530697467618818&type=3072&source=PC_COURSE_DETAIL 
+课程内容： 
+1、模板方法的具体应用思路； 
+2、容器中的迭代器运用的意义； 
+3、备忘录模式业务下的应用； 
+4、Android中onSaveInstanceState的备忘录应用。 
+预习资料链接：https://www.processon.com/view/link/630c68b60e3e74074bf05d91
+
+
+
+VIP课题：chapter-07 访问者、中介者、外观
+上课时间：2022/9/10    20:30-22:30
+上课讲师：kerwin老师
+课程链接：https://ke.qq.com/webcourse/index.html?r=1662017415646#cid=2024962&term_id=104592243&taid=13662913740858882&type=3072&source=PC_COURSE_DETAIL
+课程内容：
+1、Android体系下访问者模式的应用范围；
+2、中介者的业务应用；
+3、行为型模式的总结；
+4、结构型模式的目的性；
+5、中介者与外观的异同。
+资料链接：https://www.processon.com/view/link/63106f1a7d9c086c5f40595e
+
+
+
+
+VIP课题：chapter-08 经典的功能增强 代理、组合
+上课时间：2022/9/2  20:30-22:30
+上课讲师：kerwin老师
+课程链接：https://ke.qq.com/webcourse/index.html?r=1662107369879#cid=2024962&term_id=104592243&taid=13663214388569602&type=3072&source=PC_COURSE_DETAIL
+课程内容：
+1.静态代理与动态代理业务应用；
+2.内存插桩动态代理的实现原理与APT动态代理实现；
+3.代理模式与AOP的联系；
+4.组合模式的场景应用。
+
+预习资料链接：https://www.processon.com/view/link/6311bac71e0853187c0eb6a5
+
+
+
+
+VIP课题：chapter-09 装饰、桥接、适配器
+上课时间：2022/9/5  20:30-22:30
+上课讲师：kerwin老师
+课程链接：https://ke.qq.com/webcourse/index.html?r=1662361792976#cid=2024962&term_id=104592243&taid=13663218683536898&type=3072&source=PC_COURSE_DETAIL
+课程内容：
+1、Android下listview的适配器应用思路；
+2、ListView的BaseAdpater的桥接应用；
+3、Android与IO的装饰者的应用；
+4、23种设计模式总结，设计思路与应用场景考量。
+
+预习资料链接：https://www.processon.com/view/link/63159f7f7d9c0833ec79db6b
+
+
+
+VIP课题：chapter-10 项目结构管理~助力架构成本核算
+上课时间：2022/9/8   20:30-22:30
+上课讲师：kerwin老师
+课程链接：https://ke.qq.com/webcourse/index.html?r=1662617672070#cid=2024962&term_id=104592243&taid=13663222978504194&type=3072&source=PC_COURSE_DETAIL
+课程内容：
+1、关于架构、框架、设计模式的核心定位与思考；
+2、MVC、MVP、MVVM架构的优缺点及演进过程分析；
+3、MVI架构的演进与接入思路；
+4、关于项目架构选型的思考。
+
+预习资料链接：https://www.processon.com/view/link/631984ce0e3e74743f572e71
